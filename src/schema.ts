4867158432e28e662aa68b31a64fd6602fa067c8
visionsofparadise <matt@mattcavender.com>
2026-03-27 02:19:44 +0000

    plan-package-split

--- src/schema.ts
+++
@@ -1,9 +0,0 @@
-export interface FileInputMeta {
-	readonly input: "file" | "folder";
-	readonly mode?: "open" | "save";
-	readonly accept?: string;
-	readonly binary?: string;
-	readonly download?: string;
-}
-
-export type { ZodType as ModuleSchema } from "zod";
